e1f6040cda79dc2f6fe6ed7d7c7d4bbe4fdb2a53
Harry KR <harry@hskr.xyz>
2025-12-29 01:44:02 +0000

    fix: correct backup file extension from .sqlite3 to .db

--- motherhouse.backup.database.sh
+++ motherhouse.backup.database.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p ./backups
-scp motherhouse:/opt/docker/data/youtube_time/data/cache.db ./backups/cache.db.$(date +%Y%m%d_%H%M%S).sqlite3
+scp motherhouse:/opt/docker/data/youtube_time/data/cache.db ./backups/cache.db.$(date +%Y%m%d_%H%M%S).db
